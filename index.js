b21589bfa8a2aa5228a4dfb7b9386f01c08da516
Unknown <pe0877888@otc.edu>
2017-09-06 13:18:56 -0500

    Chalk Turned In

---
+++ index.js
@@ -0,0 +1,9 @@
+const chalk = require('chalk');
+ 
+console.log(chalk.blue('Happy') + chalk.cyan(' Birthday') + chalk.red('To You'));
+console.log(chalk.green('Happy') + chalk.magenta(' Birthday') + chalk.cyan('To You'));
+console.log(chalk.yellow.bold('Dear') + chalk.green.bold.underline(' Goose'));
+console.log(chalk.yellow('Happy') + chalk.bgMagenta(' Birthday') + chalk.yellow('To You'));
+console.log((''));
+console.log(chalk.green('Why did the programmer quit his job?'));
+console.log(chalk.red('Because he didnt get arrays!'));
